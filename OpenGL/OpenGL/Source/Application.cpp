ea273b4edc06f2b41c203c1573ab53b4168b0280
Juan Alzate <juandalzatem@gmail.com>
2022-07-22 23:38:18 -0500

    Finished GLEW setup

--- OpenGL/OpenGL/Source/Application.cpp
+++ OpenGL/OpenGL/Source/Application.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
 	/* Initialize the library */
 	if (!glfwInit())
+	{
 		return -1;
+	}
 
 	/* Create a windowed mode window and its OpenGL context */
 	GLFWwindow* Window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
@@ -17,17 +21,18 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(Window);
 
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(Window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(Window);
