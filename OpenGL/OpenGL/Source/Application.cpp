2940016059a4ede6cd4fffa9c196289033d5b4ef
Juan Alzate <juandalzatem@gmail.com>
2022-07-23 00:28:50 -0500

    Created a vertex buffer / mocked drawing a triangle

--- OpenGL/OpenGL/Source/Application.cpp
+++ OpenGL/OpenGL/Source/Application.cpp
@@ -11,15 +11,15 @@ int main(void)
 	}
 
 	/* Create a windowed mode window and its OpenGL context */
-	GLFWwindow* Window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-	if (!Window)
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
 	/* Make the window's context current */
-	glfwMakeContextCurrent(Window);
+	glfwMakeContextCurrent(window);
 
 	if (glewInit() != GLEW_OK)
 	{
@@ -28,14 +28,35 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// Generate a buffer and store its id - this will be our vertex buffer
+	unsigned int bufferId;
+	glGenBuffers(1, &bufferId);
+
+	// Select the buffer - every operation will now affect this specific buffer
+	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+
+	// Define the data that I want to bind to the buffer
+	constexpr float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	// Specify the data that the buffer has - in this case that means an array of positions that will not change and will be drawn to the screen
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(Window))
+	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Draw triangles with the specified first index and index count. This won't do anything just yet because we haven't
+		// specified what kind of data the current buffer has and we haven't specified the shaders
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
 		/* Swap front and back buffers */
-		glfwSwapBuffers(Window);
+		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
